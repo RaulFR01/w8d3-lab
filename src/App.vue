cb3677362a3bbd23e9fe05993776c990e4adb055
RaulFR01 <rferrazrobles@hotmail.com>
2022-08-27 13:07:27 +0200

    iteration 2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template lang="">
   <div>Vue 3!</div>
+  <AboutUs />
 </template>
 <script>
+import AboutUs from "./AboutUs.vue";
 export default {
   name: "App",
+  components: {
+    AboutUs,
+  },
 };
 </script>
 <style lang=""></style>
